4d5c64f3f3d20f7dec054af70c54edc57cd64c7d
Felice Forby <forby.1@gmail.com>
2018-10-13 09:51:38 +0900

    Add ability to see all lists and create new lists, and show a list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,28 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+  
+  def index
+    @list = List.new
+    @lists = List.all
+  end
+  
+  def show
+  end
+  
+  def create
+    @list = List.new(list_params)
+    @list.save
+    
+    redirect_to list_url(@list)
+  end
+  
+  private 
+  
+  def list_params
+    params.require(:list).permit(:name)
+  end
+  
+  def set_list
+    @list = List.find(params[:id])
+  end
 end
