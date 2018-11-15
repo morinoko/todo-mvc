36e52f865e0aaff314a4cf128f8f106a6e322558
Felice Forby <forby.1@gmail.com>
2018-11-16 07:37:14 +0900

    implement items controller and allow items to be added to lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,7 +7,7 @@ class ListsController < ApplicationController
   end
   
   def show
-    @item = @list.item.build # provided by has_many, creates a new item that is associated with the list
+    @item = @list.items.build # provided by has_many, creates a new item that is associated with the list
   end
   
   def create
