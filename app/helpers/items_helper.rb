f7e66ce2c0472898f08135c92ef235c08212b105
Felice Forby <forby.1@gmail.com>
2018-11-17 14:15:47 +0900

    refactor list show view with partials and helpers

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,11 @@
 module ItemsHelper
+  def li_class_for_item(item)
+    'completed' if item.complete?
+  end
+  
+  def li_for_item(item)
+    tag.li class: li_class_for_item(item), id: "item_#{item.id}" do
+      yield
+    end
+  end
 end
