dd488f2ac7ffebef4464247d61f5a94d14d1407b
Felice Forby <forby.1@gmail.com>
2018-11-17 14:39:58 +0900

    Add ability to delete list items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,13 @@ class ItemsController < ApplicationController
     redirect_to list_path(@item.list)
   end
   
+  def destroy
+    @item = Item.find_by(id: params[:id])
+    @item.destroy
+    
+    redirect_to list_path(@item.list)
+  end
+  
   private
   
   def item_params
