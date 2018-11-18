84526ece7d7a45e8f4e0793f3466bf0ec2b34751
Felice Forby <forby.1@gmail.com>
2018-11-19 07:41:21 +0900

    add password and use bcrypt for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_001231) do
+ActiveRecord::Schema.define(version: 2018_11_18_013743) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_11_18_001231) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
