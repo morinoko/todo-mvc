f6e58e7850f85647852aa01647b24fc34f5c5826
Felice Forby <forby.1@gmail.com>
2018-11-21 08:51:43 +0900

    begin setting up omniauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,32 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:user][:email])
-    
-    if user && user.authenticate(params[:user][:password])
-      session[:user_id] = user.id
+    if omniauth_data
+      # Login with Omniauth Github
+      raise omniauth_data.inspect
+      user = User.find_by(email: )
       
-      redirect_to root_path
+      # omniauth_data["info]
     else
-      render :new
+      # Normal login with username and password
+      user = User.find_by(email: params[:user][:email])
+      
+      if user && user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+        
+        redirect_to root_path
+      else
+        render :new
+      end
     end
   end
   
   def destroy
     reset_session
     redirect_to login_path
+  end
+  
+  def omniauth_data
+    request.env["omniauth.auth"]
   end
 end
