845d111f5b20408427ed5ffab44ae9db3c21ba8b
Felice Forby <forby.1@gmail.com>
2018-11-23 09:32:30 +0900

    refactor omniauth login flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,26 +3,11 @@ class SessionsController < ApplicationController
   end
   
   def create
-    if omniauth_data
-      if omniauth_email
-        # Login with Omniauth Github
-        user = User.find_by(email: omniauth_email)
-  
-        if user
-          session[:user_id] = user.id
-          redirect_to root_path
-        else
-          # Could also store the provider and their userID on the provider
-          user = User.new(email: omniauth_email, password: SecureRandom.hex)
-          user.save
-          
-          session[:user_id] = user.id
-          redirect_to root_path
-        end
-      else
-        flash[:alert] = "There was problem logging in with Github"
-        render :new
-      end
+    if auth_hash = omniauth_data
+      user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = user.id
+      
+      redirect_to root_path
     else
       # Normal login with username and password
       user = User.find_by(email: params[:user][:email])
@@ -44,9 +29,5 @@ class SessionsController < ApplicationController
   
   def omniauth_data
     request.env["omniauth.auth"]
-  end
-  
-  def omniauth_email
-    omniauth_data["info"]["email"]
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   
   validates :email, presence: true
   validates :email, uniqueness: true
+  
+  def self.find_or_create_by_omniauth(auth_hash)
+    omniauth_email = auth_hash["info"]["email"]
+    
+    self.where(email: omniauth_email).first_or_create do |user|
+      user.password = SecureRandom.hex
+      # Could also store the provider and their userID on the provider
+    end
+  end
 end
